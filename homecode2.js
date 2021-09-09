f52bd77812c3452dd7931f4d4fe9b49d917cc841
RajatMehta <rjt25881@gmail.com>
2021-09-09 20:25:02 +0530

    homecode2

---
+++ homecode2.js
@@ -0,0 +1,10 @@
+// 2 nos equal or not 
+var a=7;
+var b=5;
+a==b?console.log(`Both numbers ${a},${b} are equal`):console.log(`Both numbers ${a},${b} are not equal`);
+// Vote 
+var age=18;
+age>=18?console.log(`The person is eligible to vote`):console.log(`The person is not eligible to vote`);
+// Perfect Square
+var num=25;
+(num>=0 && Math.sqrt(num)%1==0)?console.log(`${num} is a perfect square`):console.log(`${num} is not a perfect square`);
